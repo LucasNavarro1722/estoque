0caf77debb369cf3c8a0826a6f4df23d217884d8
Giovana <giovanahessel9@gmail.com>
2025-04-26 19:06:19 -0300

    Update Historico.java

--- Controle estoque setorizado/src/Historico.java
+++ Controle estoque setorizado/src/Historico.java
@@ -1,3 +1,33 @@
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Historico {
+    private ArrayList<String> registros;
+
+
+    public Historico() {
+        this.registros = new ArrayList<>();
+    }
+
+    public void registrar(String acao, Produto produto, String setorNome) {
+        LocalDateTime agora = LocalDateTime.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss ");
+        String dataHora = agora.format(formato);
+        
+        String registro = dataHora + " | " + acao + " | Produto: " + produto.getNome() + " | Setor: " + setorNome; 
+        registros.add(registro);
+    }
+
     
+    public void mostrarHistorico() {
+        System.out.println("\n--- HISTÓRICO DE AÇÕES ---");
+        if (registros.isEmpty()) {
+            System.out.println("Nenhuma ação registrada ainda.");
+        }else{
+            for (String r : registros) {
+                System.out.println(r);
+            }
+        }
+    }
 }
