74647d4bed497a26cd6257b3adc6a75eecd23d74
Lucas Esthevam Navarro <145396884+LucasNavarro1722@users.noreply.github.com>
2025-04-26 20:26:49 -0300

    Update Historico.java

--- Controle estoque setorizado/src/Historico.java
+++ Controle estoque setorizado/src/Historico.java
@@ -1,33 +1,29 @@
-import java.util.ArrayList;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Historico {
-    private ArrayList<String> registros;
+    private String acao;
+    private String produto;
+    private int quantidade;
+    private String setor;
+    private String dataHora;
 
-
-    public Historico() {
-        this.registros = new ArrayList<>();
+    public Historico(String acao, String produto, int quantidade, String setor) {
+        this.acao = acao;
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.setor = setor;
+        this.dataHora = gerarDataHora();
     }
 
-    public void registrar(String acao, Produto produto, String setorNome) {
+    private String gerarDataHora() {
         LocalDateTime agora = LocalDateTime.now();
-        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss ");
-        String dataHora = agora.format(formato);
-        
-        String registro = dataHora + " | " + acao + " | Produto: " + produto.getNome() + " | Setor: " + setorNome; 
-        registros.add(registro);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        return agora.format(formatter);
     }
 
-    
-    public void mostrarHistorico() {
-        System.out.println("\n--- HISTÓRICO DE AÇÕES ---");
-        if (registros.isEmpty()) {
-            System.out.println("Nenhuma ação registrada ainda.");
-        }else{
-            for (String r : registros) {
-                System.out.println(r);
-            }
-        }
+    @Override
+    public String toString() {
+        return "[" + dataHora + "] " + acao + " - Produto: " + produto + " | Quantidade: " + quantidade + " | Setor: " + setor;
     }
 }
