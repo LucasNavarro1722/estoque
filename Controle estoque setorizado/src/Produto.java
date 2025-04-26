b5e0051c7f0e5ac1c54eff2ed38ef1b3e8cba08c
Lucas Esthevam Navarro <145396884+LucasNavarro1722@users.noreply.github.com>
2025-04-26 20:27:12 -0300

    Update Produto.java

--- Controle estoque setorizado/src/Produto.java
+++ Controle estoque setorizado/src/Produto.java
@@ -1,49 +1,34 @@
 public class Produto {
     private int id;
-    private int quantidade;
-    private double preco;
     private String nome;
     private String tipo;
+    private int quantidade;
+    private double preco;
 
-    public Produto(int id, int quantidade, double preco, String nome, String tipo) {
+    public Produto(int id, String nome, String tipo, int quantidade, double preco) {
         this.id = id;
-        this.quantidade = quantidade;
-        this.preco = preco;
         this.nome = nome;
         this.tipo = tipo;
+        this.quantidade = quantidade;
+        this.preco = preco;
     }
 
-    public int getId() {
-        return id;
-    }
-    
-    public int getQuantidade(){
-        return quantidade;
-    }
-
-    public double getPreco(){
-        return preco;
-    }
-
-    public String getNome(){
-        return nome;
-    }
-
-    public String getTipo(){
-        return tipo;
-    }
+    public int getId() { return id; }
+    public String getNome() { return nome; }
+    public String getTipo() { return tipo; }
+    public int getQuantidade() { return quantidade; }
+    public double getPreco() { return preco; }
 
     public void setQuantidade(int novaQuantidade) {
         this.quantidade = novaQuantidade;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return "ID: " + id +
-                " | Quantidade: " + quantidade +
-                " | Preço: " + preco +
-                " | Nome: " + nome +
-                " | Tipo: " + tipo ;
+               " | Nome: " + nome +
+               " | Tipo: " + tipo +
+               " | Quantidade: " + quantidade +
+               " | Preço: R$" + preco;
     }
-
 }
